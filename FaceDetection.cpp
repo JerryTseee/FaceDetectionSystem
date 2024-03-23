24b26729608e764d94a76ee42bffc36ae0e63430
TseWangPok_Jerry <126223772+JerryTseee@users.noreply.github.com>
2024-03-23 13:29:48 +0800

    Add files via upload

--- FaceDetection.cpp
+++ FaceDetection.cpp
@@ -2,10 +2,10 @@
 //it includes functionality for detecting objects in images or video streams, like Haar cascades, HOG, and deep learning-based methods
 #include "C:\opencv\build\include\opencv2\objdetect\objdetect.hpp"
 
-//The highgui module provides functions for creating graphical user interfaces (GUIs) and handling input/output operations. It includes functions for displaying images, creating windows, capturing video from cameras, and handling keyboard/mouse events.
+//the highgui module provides functions for creating graphical user interfaces (GUIs) and handling input/output operations. It includes functions for displaying images, creating windows, capturing video from cameras, and handling keyboard/mouse events.
 #include "C:\opencv\build\include\opencv2\highgui\highgui.hpp"
 
-//this header provides functionality for deep neural network (DNN) based operations
+//the DNN module provides functionality for deep neural network (DNN) based operations
 #include "C:\opencv\build\include\opencv2\dnn\dnn.hpp"
 
 
@@ -35,16 +35,17 @@ nestedCascadeName: Similarly, this string variable likely holds the path or name
 
 int main(int argc, const char** argv)//argc and argv allow you to handle command-line arguments
 {
-    dnn::Net ageNet;
-    ageNet = dnn::readNetFromCaffe("D:\\HKU_Resources\\C++tutorials\\C++training\\FaceDetection\\age_deploy.prototxt", "D:\\HKU_Resources\\C++tutorials\\C++training\\FaceDetection\\age_net.caffemodel");
+    dnn::Net ageNet;//declare a variable named ageNet of type dnn::Net, it represents the neural network model for age estimation.
+    //then, we load the pre-trained network from the Caffe framework.(Caffe is a deep learning framework that allow people to train neural networks for different tasks)
+    ageNet = dnn::readNetFromCaffe("D:\\HKU_Resources\\C++tutorials\\C++training\\FaceDetection\\gender_deploy.prototxt", "D:\\HKU_Resources\\C++tutorials\\C++training\\FaceDetection\\gender_net.caffemodel");//"age_deploy.prototxt" file contains the architecture of the neural network. "age_net.caffemodel" file contains the learned weights and parameters of the neural network.
+    
     // Class for video capturing from video files & provides C++ API for capturing video from cameras or reading video files.
-    VideoCapture capture;//VideoCapture class in OpenCV is used for capturing video frames from video files or cameras.
     Mat frame, image;//declare two OpenCV matrices 
 
 
     // PreDefined trained XML classifiers with facial features
-    CascadeClassifier cascade, nestedCascade;
-    //try scale range 0.1 to 2
+    CascadeClassifier faceCascade, eyeCascade;
+
     double scale = 3;//scale parameter controls the resizing of the input image during face detection. Adjusting this value can impact the detection performance.
     /*
     Increasing the scale factor makes the algorithm more sensitive to detecting smaller faces.
@@ -53,21 +54,27 @@ int main(int argc, const char** argv)//argc and argv allow you to handle command
     Decreasing the scale factor makes the algorithm less sensitive to small faces.
     It focuses on larger faces and may miss smaller faces.
     */
+
+
     // Loads a classifiers from "opencv\sources\data\haarcascades" directory
-    nestedCascade.load("C:\\opencv\\sources\\data\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml");
+    //loading eye cascade
+    eyeCascade.load("C:\\opencv\\sources\\data\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml");
+    //loading face cascade
+    faceCascade.load("C:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalcatface.xml");
 
 
-    // Before execution, change the paths according to your openCV location
-    cascade.load("C:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalcatface.xml");
 
 
     // To capture Video from WebCam replace the path with 0, else edit the path
-    capture.open("C:\\Users\\admin\\Videos\\cindy2.mp4");
+    //VideoCapture class in OpenCV is used for capturing video frames from video files or cameras.
+    VideoCapture capture("C:\\Users\\admin\\Videos\\Jerry.mp4");
+
+
     if (capture.isOpened())//check whether the file is successfully opened
     {
         // Capturing frames from the video
         cout << "Face Detection is started...." << endl;
-        while (1)
+        while ( true )
         {
             capture >> frame;//frames are captured from the video
             if (frame.empty())//if the captured frame is empty(end of video), the loop breaks
@@ -76,13 +83,13 @@ int main(int argc, const char** argv)//argc and argv allow you to handle command
             //A clone of the captured frame (frame1) is created for processing.
             Mat frame1 = frame.clone();
 
-            // Calling the detect face function
-            detectFace(frame1, cascade, nestedCascade, scale, ageNet);//to detect faces in the cloned frame using the specified cascade classifiers (cascade and nestedCascade).
+            // Calling the detect face function!!!
+            detectFace(frame1, faceCascade, eyeCascade, scale, ageNet);//to detect faces in the cloned frame using the specified cascade classifiers (cascade and nestedCascade).
             
 
-            char c = (char)waitKey(10);//The program waits for a key press
+            char key = waitKey(1);//The program waits for a key press
             // Press q to exit from the window
-            if (c == 27 || c == 'q' || c == 'Q')//If the pressed key is ．q・ or ．Q・, the loop breaks, allowing the user to exit the window.
+            if (key == 'q' || key == 'Q')//If the pressed key is ．q・ or ．Q・, the loop breaks, allowing the user to exit the window.
                 break;
         }
     }
@@ -95,12 +102,14 @@ int main(int argc, const char** argv)//argc and argv allow you to handle command
 // Defination of Detect face function
 void detectFace(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedCascade, double scale, dnn::Net& ageNet)
 {
-    vector<Rect> faces, faces2;
+    vector<Rect> faces;
     Mat gray, smallImg;//Mat gray, smallImg;: These matrices are used for intermediate processing steps. gray is typically a grayscale version of the input image, and smallImg is a resized version of the image.
 
     // To convert the frames to gray color for better face detection
     cvtColor(img, gray, COLOR_BGR2GRAY);
-    double fx = 1 / scale;//fx>1: upsampling the image. fx<1: downsampling the image.
+    equalizeHist(gray, gray);//enhance the contrast of the grayscale image
+
+    double fx = 1.109 / scale;//fx>1: upsampling the image. fx<1: downsampling the image.
 
 
     // To resize the frames, smallImg matrix will contain the resized image.
@@ -109,7 +118,7 @@ void detectFace(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedC
 
 
     // To detect faces of different sizes using cascade classifier
-    cascade.detectMultiScale(smallImg, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(10, 10));
+    cascade.detectMultiScale(smallImg, faces, 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
     /*
     faces: A vector of rectangles (bounding boxes) that will store the detected faces.
     1.1: The scale factor. It determines how much the image size is reduced at each image scale. A value greater than 1 makes the algorithm more sensitive to smaller faces.
@@ -128,12 +137,12 @@ void detectFace(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedC
         Mat smallImgROI;
         vector<Rect> nestedObjects;
         Point center;
-        Scalar color = Scalar(255, 100, 180);//this is for the circle color :), this is pink!
+        Scalar color = Scalar(0, 255, 0);//this is for the circle color
         int radius;
 
 
         double aspect_ratio = (double)r.width / r.height;
-        if (0.75 < aspect_ratio && aspect_ratio < 1.3)
+        if (0.5 < aspect_ratio && aspect_ratio < 4)
         {
             //If the aspect ratio falls within the range of 0.75 to 1.3 (approximately square face), a circle is drawn around the face
             center.x = cvRound((r.x + r.width * 0.5) * scale);
@@ -154,14 +163,14 @@ void detectFace(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedC
 
         //Age estimation part:
         Mat faceROI = img(r);//extract the face region from the image
-        //process the face ROI
-        Mat blob = dnn::blobFromImage(faceROI, 1.0, Size(227, 227), Scalar(78.4263377603, 87.7689143744, 114.895847746), false);
+        //Perform age estimation using the neural network model
+        Mat blob = dnn::blobFromImage(faceROI, 1.0, Size(227, 227), Scalar(78.4263377603, 87.7689143744, 114.895847746), false, false);
         ageNet.setInput(blob);
         //perform forward pass and get the predicted age
         Mat agePreds = ageNet.forward();
         int ageClass = max_element(agePreds.begin<float>(), agePreds.end<float>()) - agePreds.begin<float>();
         //display the predicted age
-        string ageLabel = "Age: " + std::to_string(ageClass);
+        string ageLabel = "Age: " + to_string(ageClass);
         putText(img, ageLabel, Point(img.cols/2, img.rows/2), FONT_HERSHEY_SIMPLEX, 0.9, Scalar(0, 255, 0), 2);
         //Point here is to draw the location of the display age label
     }
